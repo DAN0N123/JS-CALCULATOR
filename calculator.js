a37809b04fa37e8dd1c488b4ca42e32b4d338c02
DAN0N123 <filipskubis01@wp.pl>
2024-02-01 19:06:56 +0100

    Finish calculator

--- calculator.js
+++ calculator.js
@@ -1,22 +1,128 @@
 const buttons = document.querySelectorAll(".buttons button")
 const screen = document.querySelector(".screen")
 const minus = document.querySelector('.minus')
+let number1 = null;
+let number2 = null;
+let operator = null;
+let is_result = false;
+
+const operations = {
+    '+': (a, b) => +a + +b,
+    '-': (a, b) => a - b,
+    '×': (a, b) => +a * +b,
+    '÷': (a, b) => Math.round((a / b) * 1000) / 1000
+};
+
 for (let i = 0; i < buttons.length; i++){
-    if(String(parseInt(buttons[i].textContent)) === buttons[i].textContent){
+    buttons[i].addEventListener('mousedown', () => buttons[i].classList.add('holding'))
+    buttons[i].addEventListener('mouseup', () => buttons[i].classList.remove('holding'))
+    if(String(parseFloat(buttons[i].textContent)) === buttons[i].textContent){
         buttons[i].addEventListener('click', (event) => addNumber(event.target.textContent))
     }
     if(buttons[i].textContent === 'CE'){
-        buttons[i].addEventListener('click', () => screen.textContent = '')
+        buttons[i].addEventListener('click', clear)
     }
     if(buttons[i].textContent === '±'){
         buttons[i].addEventListener('click', reverse) 
     }
+
+    if(buttons[i].classList.contains('operator')){
+        buttons[i].addEventListener('click', (event) => operation(event.target.textContent))
+    }
+
+    if(buttons[i].textContent === '='){
+        buttons[i].addEventListener('click', equals)
+    }
+
+    if(buttons[i].textContent === '%'){
+        buttons[i].addEventListener('click', percent)
+    }
+    if(buttons[i].textContent === '.'){
+        buttons[i].addEventListener('click', comma)
+    }
 }
 
 function reverse(){
-    screen.textContent = String(parseInt(screen.textContent) * (-1))
+    screen.textContent = String(parseFloat(screen.textContent) * (-1))
 }
 
+function percent(){
+    if (screen.textContent){screen.textContent = parseFloat(screen.textContent) / 100}
+}
+function backspace(){
+
+}
+function clear(){
+    screen.textContent = '';
+    number1 = null;
+    number2 = null;
+    operator = null;
+    is_result = false;
+}
+function comma(){
+    let screenArr;
+    if (!screen.textContent.split('').includes('.')){screen.textContent += '.'}
+    
+}
+function operation(button){
+    if (operator === null){
+        operator = button;
+    }
+    
+    if (screen.textContent){
+        if (number1 === null){
+            number1 = parseFloat(screen.textContent)
+            screen.textContent = ''
+        }
+        else if (is_result){
+            number1 = parseFloat(screen.textContent)
+            screen.textContent= ''
+            operator = button;
+        }
+        else{
+            number2 = parseFloat(screen.textContent);
+            console.log(number1,number2,operator)
+            result = operations[operator](number1, number2)
+            screen.textContent = result;
+            number1 = result;
+            number2 = null;
+            is_result = true;}
+        }
+    operator = button;
+    }
+    
+
+
+
+function equals(){
+    let screenArr = screen.textContent.split('')
+    if (screenArr[screenArr.length - 1 ] === '.'){
+        return
+    }
+    if(screen.textContent && number1 && operator){
+        screen.textContent = operations[operator](number1, screen.textContent);
+        number2 = null;
+        number1 = screen.textContent;
+        operator = null;
+        is_result= true;
+    }
+    
+}
 function addNumber(whatNumber){
-    screen.textContent += whatNumber
+    if (screen.textContent.length === 0 && whatNumber === '0'){
+        console.log(screen.textContent.length)
+        return
+    }
+    if(screen.textContent.length < 18){
+        if (!is_result){screen.textContent += whatNumber}
+        else if (operator === null){
+        screen.textContent = whatNumber;
+        is_result = false;
+        number2 = null;
+        number1 = null;
+        }
+        else{
+            screen.textContent += whatNumber;
+        }}
+    
 }
